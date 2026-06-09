c0cef2b60d64db4d5877502b1f6c7314b7dbe47b
Robert <r.niyonkuru@alustudent.com>
2026-06-10 00:27:39 +0200

    feat: Displaying the table items in the apk

--- PyQtDB-tut/todo_table.py
+++ PyQtDB-tut/todo_table.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem
+from PyQt6.QtSql import QSqlQuery
 
 class TodoTable(QWidget):
     def __init__(self):
@@ -16,6 +17,19 @@ class TodoTable(QWidget):
 
         layout.addWidget(self.todo_table)
         self.setLayout(layout)
+        self.get_todos()
+
+    def get_todos(self):
+        query = QSqlQuery()
+        query.exec("SELECT name, completed FROM todos")
+
+        while query.next():
+            rows = self.todo_table.rowCount()
+            self.todo_table.setRowCount(rows + 1)
+            self.todo_table.setItem(rows, 0, QTableWidgetItem(query.value(0)))
+            completed = query.value(1)
+            status = "Completed" if completed else "Pending"
+            self.todo_table.setItem(rows, 1, QTableWidgetItem(status))
 
     def add_todo_item(self, name, completed):
         # Determine the current row count and insert a new row
